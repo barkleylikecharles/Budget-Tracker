110f04a776320ec851df86d89551bc9c79b6c5d4
Nicole Barkley <barkleylikecharles@icloud.com>
2022-06-17 22:11:07 -0400

    read-me

--- public/js/idb.js
+++ public/js/idb.js
@@ -16,10 +16,10 @@ request.onsuccess = function(event) {
     // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
     db = event.target.result;
   
-    // check if app is online, if yes run uploadPizza() function to send all local db data to api
+    // check if app is online
     if (navigator.onLine) {
       // we haven't created this yet, but we will soon, so let's comment it out for now
-    //   checkDatabase();
+      checkDatabase();
     }
   };
   
@@ -28,7 +28,7 @@ request.onsuccess = function(event) {
     console.log(event.target.errorCode);
   };
 
-  // This function will be executed if we attempt to submit a new pizza and there's no internet connection
+  // This function will be executed if we attempt to submit a new transaction and there's no internet connection
 function saveRecord(record) {
     // open a new transaction with the database with read and write permissions 
     const transaction = db.transaction(['new_transaction'], 'readwrite');
